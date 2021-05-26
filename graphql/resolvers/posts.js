3d5ac679a0e49c67f84a60877dde123c6069bca6
unknown <bittugautam1224@gmail.com>
2021-05-26 18:34:47 +0530

    Authentication and Posts

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,64 @@
+const {AuthenticationError}= require('apollo-server');
+
+const Post = require('../../models/Post');
+const checkAuth=require('../../utils/check-auth');
+
+module.exports= {
+    Query: {
+        async getPosts() {
+            try{
+                const posts = await Post.find();
+                return posts;
+            }catch(e){
+                throw new Error(e);
+            }
+        },
+        async getPost(_,{postId}){
+            try {
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                }
+                else{
+                    throw new Error('Post not found');
+                }
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_,{body},context){
+            const user = checkAuth(context);
+            console.log(user);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString,
+            });
+            const post = await newPost.save();
+
+            return post;
+        },
+
+        async deletePost(_,{postId},context){
+            const user = checkAuth(context);
+
+            try {
+                
+                const post = await Post.findById(postId);
+                if(user.username=== post.username){
+                    await post.delete();
+                    return 'Post Deleted Sucessfully';
+                }
+                else{
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    }
+}
