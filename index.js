a75b312dc0450747742a200c0ab826ac8704e7ff
unknown <bittugautam1224@gmail.com>
2021-05-31 01:40:29 +0530

    PubSub Added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {ApolloServer}= require('apollo-server');
+const {ApolloServer, PubSub}= require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
@@ -12,7 +12,7 @@ const resolvers= require('./graphql/resolvers');
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({req})=>({req})
+    context: ({req})=>({req,pubsub})
 });
 
 mongoose
